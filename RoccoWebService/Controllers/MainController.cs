20d70b85505f299fe76c456d96f9d6623afc0c9f
Lenny Evans <lenny.evans3@gmail.com>
2017-03-05 17:37:09 -0600

    Cleanup AddToGoogleCalendar as private method and modify original same named action to call it with specific parameters. Tested, working.

--- RoccoWebService/Controllers/MainController.cs
+++ RoccoWebService/Controllers/MainController.cs
@@ -24,11 +24,19 @@ namespace RoccoWebService.Controllers {
         // Add item to Google Calendar using service account
         // Be sure to share the calendar with the service account just like a regular user
         [System.Web.Http.HttpGet]
+        [Route("api/AddToGoogleCalendar")]
         public IHttpActionResult AddToGoogleCalendar() {
+            AddToGoogleCalendar("Summary stuff", "This is the description.", new DateTime(2017, 3, 5, 14, 0, 0), new DateTime(2017, 3, 5, 14, 0, 0), 1, "11");
+            return Ok();
+        }
+
+
+
+        private void AddToGoogleCalendar(string summary, string description, DateTime startDateTime, DateTime endDateTime, int reminderMinutes, string eventColorId) {
             String serviceAccountEmail = File.ReadAllText(HostingEnvironment.MapPath(@"~/App_Data/serviceAccountEmail"));
 
             // Read certificate
-            // @ is for verbatim string literal as opposed to regular (prevent taking meaning from characters in key)
+            // @ is for verbatim string literal as opposed to regular string literal (prevent taking meaning from characters in key)
             var certificate = new X509Certificate2(HostingEnvironment.MapPath(@"~/App_Data/key.p12"), "notasecret", X509KeyStorageFlags.Exportable);
 
             // Create credential from certificate
@@ -43,45 +51,50 @@ namespace RoccoWebService.Controllers {
                 ApplicationName = "RoccoWebService",
             });
 
-            // Create the calendar event object
+            // Create the calendar event
             Event calendarEvent = new Event();
+            calendarEvent.Summary = summary;
+            calendarEvent.Description = description;
 
-            calendarEvent.Summary = "Google I/O 2015";
-            calendarEvent.Location = "800 Howard St., San Francisco, CA 94103";
-            calendarEvent.Description = "A chance to hear more about Google's developer products.";
-
-            DateTime startDateTime = new DateTime(2017, 3, 4, 14, 0, 0); //("2015-05-28T09:00:00-07:00");
+            // Start date and time
             EventDateTime start = new EventDateTime();
             start.DateTime = startDateTime;
-            start.TimeZone = "America/Los_Angeles";
+            start.TimeZone = "America/Chicago";
             calendarEvent.Start = start;
 
-            DateTime endDateTime = new DateTime(2017, 3, 4, 14, 0, 0);
+            // End date and time
             EventDateTime end = new EventDateTime();
             end.DateTime = endDateTime;
-            end.TimeZone = "America/Los_Angeles";
+            end.TimeZone = "America/Chicago";
             calendarEvent.End = end;
 
-            String[] recurrence = new String[] { "RRULE:FREQ=DAILY;COUNT=2" };
-            calendarEvent.Recurrence = recurrence;
-
-            var attendees = new List<EventAttendee>() {
-                new EventAttendee() { Email = "nobodyhome1@lennysmadeupserver.com" },
-                new EventAttendee() { Email = "nobodyhome2@lennysmadeupserver.com" }
-            };
-            calendarEvent.Attendees = attendees;
-
+            // Reminders and notifications
             var remindersList = new List<EventReminder>() {
-                new EventReminder() { Method = "email", Minutes = (24 * 60) },
-                new EventReminder() { Method = "popup", Minutes = 10 }
+                new EventReminder() { Method = "popup", Minutes = reminderMinutes }
             };
             var reminders = new Event.RemindersData { UseDefault = false, Overrides = remindersList };
             calendarEvent.Reminders = reminders;
 
+            // Event color list
+            // 1 = light blue
+            // 2 = light green
+            // 3 = levender
+            // 4 = salmon
+            // 5 = yellow
+            // 6 = tan
+            // 7 = aqua
+            // 8 = gray
+            // 9 = blue
+            // 10 = green
+            // 11 = red
+            
+            // Set event color
+            calendarEvent.ColorId = eventColorId;
+
             var calendarId = File.ReadAllText(HostingEnvironment.MapPath(@"~/App_Data/calendarId"));
             var eventInsert = service.Events.Insert(calendarEvent, calendarId).Execute();
 
-            return Ok();
+            //return Ok();
         }
     }
 }
